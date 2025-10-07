5af3317c6fd83341ee8fbbcef3f6328decd9c4d2
Ultrafish19 <romonm2008@gmail.com>
2025-10-07 11:19:59 -0400

    Made audio working. Will attempt to add pausing and other media controls.

--- MusicService.py
+++ MusicService.py
@@ -1,10 +1,9 @@
 # Opensound Music Service Module
 
-import pytubefix # Install the main module for it
 from pytubefix import YouTube # Import YouTube from the module
 import os # Import the OS module to handle file paths
-from playsound import playsound as Playsound # Import the playsound module to play audio
-
+import pydub # Audio libarary to handle audio files
+from pydub.playback import play # Import the playback function from pydub
 if __name__ == "__main__": # This will prevent the file from being run directly
     print("This is a module, and should not be run directly.") # Warn the user if they try to run this file directly
     exit() # Exit if this file is run directly
@@ -17,9 +16,15 @@ CachePath = R"C:\Users\rmohamma1884\OneDrive - Hamilton Wentworth District Schoo
 SongName = R"\Song.m4a"
 
 
+pydub.AudioSegment.converter = R"C:\Users\rmohamma1884\Downloads\ffmpeg-8.0-essentials_build\ffmpeg-8.0-essentials_build\bin\ffmpeg" # THIS WILL CHANGE ON LINUX 
 
 
-def PlaySong(Link): #This will download a the song from Youtube music, and play it.
+def PlaySong(AudioPath): # This will play the song from the cache folder
+    Audio = pydub.AudioSegment.from_file(AudioPath, codec="aac") # Load the audio file from the cache folder and play the AAC codec
+    play(Audio) # Play the audio file
+
+
+def FetchSong(Link): #This will download a the song from Youtube music, and play it.
 
     if os.path.exists(CachePath+SongName): # Check if the song already exists in the cache folder
         os.remove(CachePath+SongName) # Remove the old song from the cache folder if it exists
@@ -28,7 +33,7 @@ def PlaySong(Link): #This will download a the song from Youtube music, and play 
     Stream = YoutubeSong.streams.get_audio_only() # Get the audio only stream
     AudioDownload = Stream.download(output_path=CachePath) # Download the audio to the cache folder
     os.rename(AudioDownload, CachePath+SongName) # Rename the file to have a .mp4 extension, the variable will also be changed
-    Playsound(CachePath+SongName) # Play the song from the cache folder
+    PlaySong(CachePath+SongName) # Play the song from the cache folder
 
 
 
--- OpenSound.py
+++ OpenSound.py
@@ -7,5 +7,5 @@ import MusicService # Import the Music Service module
 
 
 while True:
-    MusicService.PlaySong(input("Enter a Youtube Link: ")) # Ask the user for a link, and play the song
+    MusicService.FetchSong(input("Enter a Youtube Link: ")) # Ask the user for a link, and play the song
 
