664fc2722b99adfae1bc0121d22cbd91500a200a
Ultrafish19 <romonm2008@gmail.com>
2025-10-09 11:46:27 -0400

    Improved playback more and added search and replay.

--- MusicService.py
+++ MusicService.py
@@ -1,25 +1,44 @@
 # Opensound Music Service Module
 
 import pygame
-from pytubefix import YouTube # Import YouTube from the module
+from pytubefix import YouTube # Import YouTube and Search from the module
+from pytubefix.contrib.search import Search, Filter
 import os # Import the OS module to handle file paths
 import pydub # Audio libarary to handle audio files
 from pydub.playback import play # Import the playback function from pydub
 from pydub import AudioSegment
+
+
+
 if __name__ == "__main__": # This will prevent the file from being run directly
     print("This is a module, and should not be run directly.") # Warn the user if they try to run this file directly
     exit() # Exit if this file is run directly
 
 
+
+
+
+
+
+
+
+
+
 CachePath = R"C:\Users\rmohamma1884\OneDrive - Hamilton Wentworth District School Board\Documents\OpenSound\OpenSound\Cache\Music"
 SongName = R"\Song.m4a" # .M4a name
 ConvertedSongName = R"\Song.wav" # Name of song when converted to .WAV
-
+MAXLENGTH = 600 # The maximum song length in seconds
 
 pydub.AudioSegment.converter = R"C:\Users\rmohamma1884\Downloads\ffmpeg-8.0-essentials_build\ffmpeg-8.0-essentials_build\bin\ffmpeg" # THIS WILL CHANGE ON LINUX 
 
 
-def ConvertCodec(YoutubeAudioPath): # This will convert from .M4A to .WAV which works on Pygame
+class SongTooLongError(Exception):
+    def __init__(Self,Message=f"Your song is too long, the maximum length is {MAXLENGTH} secs"):
+        super().__init__(Message)
+
+
+
+def ConvertCodec(YoutubeAudioPath : str): # This will convert from .M4A to .WAV which works on Pygame
     try:
         M4AAudio = pydub.AudioSegment.from_file(YoutubeAudioPath, codec="aac")
         M4AAudio.export(YoutubeAudioPath.replace(".m4a", ".wav"), format="wav") # Export the audio file as a .WAV file
@@ -40,32 +59,60 @@ def SetAudioPlaying(SetTo : bool):
         pygame.mixer.music.pause() # Pause the audio if SetTo is False
 
 
-def PlaySong(AudioPath): # This will play the song from the cache folder
+def PlaySong(AudioPath, AlreadyConverted = False): # This will play the song from the cache folder
 
-    ConvertedPath = ConvertCodec(AudioPath) # Convert the audio file to .WAV
 
-    if ConvertedPath is None: # Check if the conversion was successful
-        print("Failed to convert audio file.") # Print an error message if the conversion failed
-        return # Exit the function if the conversion failed
+    if AlreadyConverted == False:
+        ConvertedPath = ConvertCodec(AudioPath) # Convert the audio file to .WAV
+
+        if ConvertedPath is None: # Check if the conversion was successful
+            print("Failed to convert audio file.") # Print an error message if the conversion failed
+            return # Exit the function if the conversion failed
+    else:
+        ConvertedPath = AudioPath
 
     pygame.mixer.music.load(ConvertedPath) # Load the song from the cache folder
     pygame.mixer.music.play() # Play the song
     
 
+def SearchSong(SongName : str) :  # Music searching, Returns list of Youtube videos
+
+    SongFilter = {
+        "Type" : Filter.get_type("Video"),
+        "duration" : Filter.get_duration("Under 4 minutes")
+    }
 
 
 
-def FetchSong(Link): #This will download a the song from Youtube music, and play it.
 
-    if os.path.exists(CachePath+SongName): # Check if the song already exists in the cache folder
-        os.remove(CachePath+SongName) # Remove the old song from the cache folder if it exists
+    Results = Search(SongName, filters=SongFilter) # Search song with the given keywords
 
-    if os.path.exists(CachePath+ConvertedSongName): # Check if the converted song already exists in the cache folder
-        pygame.mixer.music.unload() # Unload the previous song if there was one
-        os.remove(CachePath+ConvertedSongName) # Remove the converted song if it exists
 
-    YoutubeSong = YouTube(Link) # Get the Youtube link  
-    Stream = YoutubeSong.streams.get_audio_only() # Get the audio only stream
-    AudioDownload = Stream.download(output_path=CachePath) # Download the audio to the cache folder
-    os.rename(AudioDownload, CachePath+SongName) # Rename the file to have a .mp4 extension, the variable will also be changed
-    PlaySong(CachePath+SongName) # Play the song from the cache folder
+    return Results.videos
+
+
+    
+    
+
+
+def FetchSong(Link : str): #This will download a the song from Youtube music, and play it.
+
+    try:
+        if os.path.exists(CachePath+SongName): # Check if the song already exists in the cache folder
+            os.remove(CachePath+SongName) # Remove the old song from the cache folder if it exists
+
+        if os.path.exists(CachePath+ConvertedSongName): # Check if the converted song already exists in the cache folder
+            pygame.mixer.music.unload() # Unload the previous song if there was one
+            os.remove(CachePath+ConvertedSongName) # Remove the converted song if it exists
+
+        YoutubeSong = YouTube(Link) # Get the Youtube link  
+
+        if YoutubeSong.length > MAXLENGTH: # Prevent anyone from downloading a 24 hour long video.
+            raise SongTooLongError
+
+        Stream = YoutubeSong.streams.get_audio_only() # Get the audio only stream
+        AudioDownload = Stream.download(output_path=CachePath) # Download the audio to the cache folder
+        os.rename(AudioDownload, CachePath+SongName) # Rename the file to have a .mp4 extension, the variable will also be changed
+        PlaySong(CachePath+SongName) # Play the song from the cache folder
+    except Exception as e:
+        print(f"Failed to download song. \n {e}")
--- OpenSound.py
+++ OpenSound.py
@@ -7,11 +7,16 @@ MusicService.InitAudio() # Initialize the audio system
 
 
 print("Welcome to OpenSound!") # Welcome message
-print("Commands are: \n <URL> to play a song \n Pause to pause \n Play to resume")
+print("Commands are: \n <URL> to play a song \n Pause to pause \n Play to resume \n Replay to play the cached song")
+
+
+
+
+
 
 
 while True:
-    UserInput = input("Enter a command or URL: ") # Ask the user for a command or URL
+    UserInput = input("Enter a command or Song name: ") # Ask the user for a command or URL
 
     if UserInput.lower() == "pause": # If the user wants to pause the audio
         MusicService.SetAudioPlaying(False) # Pause the audio
@@ -19,6 +24,20 @@ while True:
     elif UserInput.lower() == "play": # If the user wants to resume the audio
         MusicService.SetAudioPlaying(True) # Resume the audio
         print("Audio resumed.") # Inform the user that the audio is resumed
+    elif UserInput.lower() == "replay":
+        MusicService.PlaySong(MusicService.CachePath + MusicService.ConvertedSongName,True)
     else: # Otherwise, assume the user entered a URL
-        MusicService.FetchSong(UserInput) # Ask the user for a link, and play the song
+        
+
+        try:
+            SearchResults = MusicService.SearchSong(UserInput)
+
+            SearchResult = SearchResults[0] # First song on list
+
+            print(f"Playing {SearchResult.title}, by {SearchResult.author}.")
+
+            MusicService.FetchSong(SearchResult.watch_url) # Play video link
+        except Exception as e:
+            print(f"Something went wrong, please try again. \n {e}")
+
 
